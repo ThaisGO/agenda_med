7359ade66d776d263861ad518a30c43fe0f214a9
thaisgo <thaisgo.viana@gmail.com>
2026-05-01 18:47:22 -0300

    feat: add stores with pinia

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+  import { onMounted } from 'vue'
+  import { RouterView } from 'vue-router'
+  
+  import { useAuthStore } from '@/stores/auth'
+
+  const auth = useAuthStore()
+
+  onMounted(() => {
+    auth.fetchUser()
+  })
 </script>
 
 <template>
   <RouterView />
 </template>
 
-<style scoped>
 
-</style>
+
+
+
---
+++ src/stores/auth.ts
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia'
+import { supabase } from '@/lib/supabase'
+import type { Profile } from '@/types/user'
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        user: null as any,
+        profile: null as Profile | null,
+    }),
+
+    actions: {
+        async login(email: string, password: string) {
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email,
+                password
+            })
+
+            if (error) throw error
+
+            this.user = data.user
+
+            if (this.user) {
+                await this.fetchProfile()
+            }
+        },
+
+        async fetchUser() {
+            const { data } = await supabase.auth.getUser()
+                this.user = data.user
+
+            if (this.user) {
+                await this.fetchProfile()
+                
+            }
+        },
+
+        async fetchProfile() {
+            const { data } = await supabase
+                .from('profiles')
+                .select('*')
+                .eq('id', this.user.id)
+                .single()
+
+            this.profile = data
+            console.log("profile", this.profile);
+        },
+
+        async logout() {
+            await supabase.auth.signOut()
+            this.user = null
+            this.profile = null
+            // router.push('/')
+        }
+    }
+})
