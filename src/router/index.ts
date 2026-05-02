abf3239d394618953664e3adaaf6d21b1b769c43
thaisgo <thaisgo.viana@gmail.com>
2026-05-01 21:01:16 -0300

    fix: delete useless imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { supabase } from '@/lib/supabase.js'
-// import HomeView from '@/views/HomeView.vue'
-import DashboardView from '@/views/DashboardView.vue'
-import  AccountDetailsView  from '@/views/AccountDetailsView.vue'
+
 import LoginView from '@/views/LoginView.vue'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 
@@ -18,10 +16,10 @@ const router = createRouter({
           path: 'dashboard',
           component: () => import('@/views/DashboardView.vue')
         },
-        // {
-        //   path: 'appointments',
-        //   component: () => import('@/views/AppointmentsView.vue')
-        // },
+        {
+          path: 'appointments',
+          component: () => import('@/views/AppointmentsView.vue')
+        },
         {
           path: 'account',
           component: () => import('@/views/AccountDetailsView.vue')
@@ -37,17 +35,14 @@ router.beforeEach(async (to) => {
   const isAuthenticated = !!data.session
   const isAppRoute = to.path.startsWith('/app')
 
-  //se tentar acessar /app sem login → manda pro login
   if (isAppRoute && !isAuthenticated) {
     return '/'
   }
 
-  // 🔁 se já estiver logado e tentar ir pro login → manda pro dashboard
   if (to.path === '/' && isAuthenticated) {
     return '/app/dashboard'
   }
 
-   // ✅ libera navegação
   return true
 })
 
